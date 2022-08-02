46f76e608a744d7b6e9781c2b55f95385a5563e2
Sergio Velasquez <sergio.velasquez@roofstock.com>
2022-08-02 14:41:42 -0600

    add fluent validation

---
+++ AuthSettings/Validators/EmptyFieldsValidator.cs
@@ -0,0 +1,15 @@
+﻿using AuthSettings.Models;
+using FluentValidation;
+
+namespace AuthSettings.Validators;
+
+public class EmptyFieldsValidator : AbstractValidator<Settings>
+{
+    public EmptyFieldsValidator()
+    {
+        RuleFor(s => s.ClientId).NotEmpty();
+        RuleFor(s => s.LogoUri).NotEmpty();
+        RuleFor(s => s.AllowedOrigins).NotEmpty();
+        RuleForEach(s => s.AllowedOrigins).NotEmpty();
+    }
+}
