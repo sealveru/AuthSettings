ef93d0350ba76872ca0d111e4f6e3f07434f23a7
Sergio Velasquez <sergio.velasquez@roofstock.com>
2022-08-02 16:51:06 -0600

    add cli

--- AuthSettings.Cli/Program.cs
+++ AuthSettings.Cli/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AuthSettings;
 
-Console.WriteLine("Hello, World!");
+try
+{
+    var reader = new FileReader();
+    var runner = new ValidationRunner();
+
+    var settings = reader.ReadAllFiles();
+
+    foreach (var setting in settings)
+    {
+        Console.WriteLine($"\nValidating {setting.ClientId}");
+
+        var result = runner.Validate(setting).ToList();
+        result.ForEach(r => Console.WriteLine(r.ToString()));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Unexpected error. Message: {e.Message}");
+}
+
+Console.ReadKey();
