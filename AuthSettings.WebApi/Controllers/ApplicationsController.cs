90771efcc8d91a1575a607750a26c4a8af1a803e
Sergio Velasquez <sergio.velasquez@roofstock.com>
2022-08-02 16:51:26 -0600

    add web api

---
+++ AuthSettings.WebApi/Controllers/ApplicationsController.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSettings.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApplicationsController : ControllerBase
+{
+    private readonly IFileReader _reader;
+    private readonly IValidationRunner _runner;
+
+    public ApplicationsController(IFileReader reader, IValidationRunner runner)
+    {
+        _reader = reader;
+        _runner = runner;
+    }
+
+
+    [HttpGet]
+    public IEnumerable<Application> GetApplications()
+    {
+        var settings = _reader.ReadAllFiles();
+        return settings.Select(s => new Application(s.ClientId, s.Name));
+    }
+    
+    [HttpGet("{id}/validations")]
+    public IEnumerable<ValidationResult>? GetValidations(string id)
+    {
+        var setting = _reader.ReadAllFiles().FirstOrDefault(s => s.ClientId == id);
+        return _runner.Validate(setting);
+    }
+}
+
+public record Application(string ClientId, string Name);
